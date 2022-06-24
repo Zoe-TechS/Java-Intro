33dc63cf52059c4131701a9068a7942eded4943a
unknown <osatoama@gmail.com>
2022-06-24 04:32:42 +0100

    First Commit

---
+++ details.js
@@ -0,0 +1,8 @@
+let myName = 'Osatohanmwen Amayo'
+let myHeight = "5'6inches"
+let myCountry = "Nigeria"
+
+alert("Name: " + myName + "\n" + 
+      "Height: " + myHeight + "\n" + 
+      "Country: " + myCountry
+);
